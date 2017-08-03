d97960099d39cf3ca320bbea8dbce9b070329e0c
Tekkit886 <brian@tailboardtech.com>
2017-08-03 09:39:02 -0400

    Updated feature added to portfolio tiems

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,10 +15,27 @@ class PortfoliosController < ApplicationController
         format.html { redirect_to @portfolio_item, notice: 'Portfolio Item is now live!' }
 
       else
-        format.html { render :new }
-        
+        format.html { render :new}
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(parms[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(parms[:id])
+    respond_to do |format|
+      if @portfolio_item.update(blog_params)
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio Item was successfully updated.' }
+
+      else
+        format.html { render :edit }
+
+      end
+    end
+  end
+
 end
 
