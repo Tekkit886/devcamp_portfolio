edfe91fd77ef1689163e72abc78c7d1930d8aad2
Tekkit886 <brian@tailboardtech.com>
2017-08-03 22:32:33 -0400

    intergrated friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
-  get 'about', to 'pages#about'
-  get 'contact', to 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
 
 
