d59f69b441f83dece637ce7b149e6e0519c56f46
Tekkit886 <brian@tailboardtech.com>
2017-09-23 21:26:39 -0400

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    TailboardViewTool::Renderer.copyright 'Brian Martin', 'All rights reserved'    
+  end
 end
