bf82030860d9a63ce0873a53b6b5bf1b0d103ac4
Brian <brian@tailboardtech.com>
2018-05-03 21:43:54 -0400

    added blog styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
-	def login_helper style = ''
-    if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    else
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
-    end
-  end
+#	def login_helper style = ''
+ #   if current_user.is_a?(User)
+#      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+ #   else
+ #     (link_to "Register", new_user_registration_path, class: style) +
+ #     " ".html_safe +
+ #     (link_to "Login", new_user_session_path, class: style)
+# #   end
+#  end
 
   def source_helper(layout_name)
     if session[:source]
